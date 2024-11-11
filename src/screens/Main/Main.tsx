f78f9c5674a77a07d2f0b3cbd8d5b6f4d7c5415b
Parth Narielwala <pnarielwala@gmail.com>
2024-11-11 14:11:35 -0600

    feat(version): add modal to show app information
    fix: update verion error message logs

--- src/screens/Main/Main.tsx
+++ src/screens/Main/Main.tsx
@@ -1,5 +1,7 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Image, SafeAreaView, Pressable, Icon, Text } from 'design'
+
+import * as Updates from 'expo-updates'
 
 import Dropbox from 'assets/dropbox.svg'
 import iCloud from 'assets/icloud.svg'
@@ -12,11 +14,19 @@ import { getFolderContents } from 'api/dropboxClient'
 import { useQueryClient } from '@tanstack/react-query'
 import { ScreenPropsT } from 'App'
 import useDropBoxAuth from 'hooks/useDropboxAuth'
+import { Dialog } from 'react-native-elements'
 
 export type PropsT = ScreenPropsT<'Home'>
 
 const Main = (props: PropsT) => {
   const queryClient = useQueryClient()
+  const {
+    currentlyRunning,
+    isUpdatePending,
+    downloadError,
+    checkError,
+    initializationError,
+  } = Updates.useUpdates()
 
   const { authenticate } = useDropBoxAuth({
     onCheckAuth: async (authenticated) => {
@@ -40,9 +50,20 @@ const Main = (props: PropsT) => {
     },
   })
 
+  const [isInfoShown, setIsInfoShown] = useState(false)
+
+  useEffect(() => {
+    Updates.checkForUpdateAsync()
+  }, [])
+
   useEffect(() => {
     props.navigation.setOptions({
       headerTransparent: true,
+      headerRight: () => (
+        <Pressable onPress={() => setIsInfoShown(true)}>
+          <FontAwesome5 name="info-circle" size={24} color="black" />
+        </Pressable>
+      ),
     })
   }, [])
 
@@ -57,6 +78,57 @@ const Main = (props: PropsT) => {
         justifyContent: 'center',
       }}
     >
+      <Dialog isVisible={isInfoShown}>
+        <View sx={{ width: '100%' }}>
+          <Dialog.Title title="Version" />
+          <Text variant="bodySmall">
+            Runtime: {currentlyRunning.runtimeVersion}
+          </Text>
+          <Text variant="bodySmall">
+            Channel: {currentlyRunning.channel || 'Not set'}
+          </Text>
+          <Text variant="bodySmall">
+            Update Id: {currentlyRunning.updateId || 'Not set'}
+          </Text>
+          {downloadError && (
+            <Text variant="bodySmall" sx={{ color: 'red' }}>
+              Error downloading update: {downloadError.message}
+            </Text>
+          )}
+          {checkError && (
+            <Text variant="bodySmall" sx={{ color: 'red' }}>
+              Error checking for update: {checkError.message}
+            </Text>
+          )}
+          {initializationError && (
+            <Text variant="bodySmall" sx={{ color: 'red' }}>
+              Error initializing updates: {initializationError.message}
+            </Text>
+          )}
+          <View
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              mt: 3,
+              justifyContent: 'flex-end',
+              gap: 3,
+            }}
+          >
+            {isUpdatePending ? (
+              <Dialog.Button
+                onPress={() => Updates.fetchUpdateAsync()}
+                testID="close-info"
+                title={'Reload app'}
+              />
+            ) : null}
+            <Dialog.Button
+              onPress={() => setIsInfoShown(false)}
+              testID="close-info"
+              title={'Close'}
+            />
+          </View>
+        </View>
+      </Dialog>
       <Image
         sx={{
           width: '100%',
