d4fab899c65b5b0a34c7d8924418e0b72cd05258
Parth Narielwala <pnarielwala@users.noreply.github.com>
2026-04-29 23:37:17 -0500

    fix(ios): strip bitcode from SpotifyiOS framework (#77)

--- plugins/spotify-config-plugin/index.js
+++ plugins/spotify-config-plugin/index.js
@@ -1,8 +1,10 @@
 const withSpotifyIos = require('./withSpotifyIos')
+const withSpotifyBitcodeStrip = require('./withSpotifyBitcodeStrip')
 const withSpotifyAndroid = require('./withSpotifyAndroid')
 
 const withSpotify = (config) => {
   config = withSpotifyIos(config)
+  config = withSpotifyBitcodeStrip(config)
   config = withSpotifyAndroid(config)
   return config
 }
